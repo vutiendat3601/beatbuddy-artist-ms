f473148172ae75be4f22b90949115504a11486e1
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-31 10:14:50 +0000

    chore: initial commit

---
+++ src/main/java/vn/io/vutiendat3601/beatbuddy/artist/ArtistServiceApplication.java
@@ -0,0 +1,11 @@
+package vn.io.vutiendat3601.beatbuddy.artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistServiceApplication.class, args);
+	}
+}

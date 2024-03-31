18277b2b19347728e9bdb06861689376ae57a351
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-03-31 17:23:05 +0000

    chore: fix environment config

--- src/test/java/vn/io/vutiendat3601/beatbuddy/artist/ArtistServiceApplicationTests.java
+++ src/test/java/vn/io/vutiendat3601/beatbuddy/artist/ArtistServiceApplicationTests.java
@@ -2,12 +2,12 @@ package vn.io.vutiendat3601.beatbuddy.artist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 class ArtistServiceApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }

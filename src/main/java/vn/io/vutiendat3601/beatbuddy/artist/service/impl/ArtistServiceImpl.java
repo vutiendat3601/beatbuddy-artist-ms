76f0a974bfd1939b531f485399eb03db99eadc34
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-04-16 15:19:16 +0000

    feat: enable compression

--- src/main/java/vn/io/vutiendat3601/beatbuddy/artist/service/impl/ArtistServiceImpl.java
+++ src/main/java/vn/io/vutiendat3601/beatbuddy/artist/service/impl/ArtistServiceImpl.java
@@ -3,10 +3,13 @@ package vn.io.vutiendat3601.beatbuddy.artist.service.impl;
 import static vn.io.vutiendat3601.beatbuddy.artist.constant.ArtistConstant.ARTIST;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import vn.io.vutiendat3601.beatbuddy.artist.dto.ArtistDto;
 import vn.io.vutiendat3601.beatbuddy.artist.exception.ResourceNotFoundException;
 import vn.io.vutiendat3601.beatbuddy.artist.mapper.ArtistMapper;
@@ -28,6 +31,9 @@ public class ArtistServiceImpl implements ArtistService {
 
   @Override
   public Flux<ArtistDto> getSeveralArtists(List<String> ids) {
-    return Flux.fromIterable(ids).flatMap(this::getArtist).onErrorContinue((e, o) -> {});
+    return Flux.fromIterable(ids)
+        .flatMap(this::getArtist)
+        .onErrorContinue((e, o) -> {})
+        .subscribeOn(Schedulers.parallel());
   }
 }
